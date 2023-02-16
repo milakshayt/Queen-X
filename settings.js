1316a97835eb145259102d8379528bdda45e3277
milakshayt <125524591+milakshayt@users.noreply.github.com>
2023-02-16 07:50:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94719225807'] //ur owner number
-global.ownername = "©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
-global.ytname = "ʏᴜʀᴇꜱʜ" //ur yt chanel name
+global.owner = ['94776054222'] //ur owner number
+global.ownername = " RMilaksha ᴜꜱᴇʀ ʙᴏᴛ" //ur owner name
+global.ytname = "RMILAKSHA BoT" //ur yt chanel name
 global.socialm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.ownernomer = "94719225807" //ur number
-global.premium = ['94719225807'] //ur premium number
-global.botname = '©Qᴜᴇᴇɴ-x ᴡᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
-global.linkz = "https://chat.whatsapp.com/Fd5MPEBn7NQ1aviL4V5fa7" //your theme url which will be displayed on whatsapp
+global.ownernomer = "94776054222" //ur number
+global.premium = ['94776054222'] //ur premium number
+global.botname = "RMilaksha ᴜꜱᴇʀ ʙᴏᴛ' //ur bot name
+global.linkz = "https://www.youtube.com/channel/UC9hz2I8hN04HocGj8_LypjQ" //your theme url which will be displayed on whatsapp
 global.websitex = "https:// no" //ur website to be displayed
 global.botscript = 'https://github.com/yureshkav/Queen-X' //script link
 global.themeemoji = "🛸" //ur theme emoji
-global.packname = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻" //ur sticker watermark packname
-global.author = "   👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
-global.wm = "👸🏻ＱＵＥＥＮ-Ｘ ＢＯＴ👸🏻." //ur watermark
+global.packname = "   👸👸RMilakshsa BoT❤🌻🏻🏻" //ur sticker watermark packname
+global.author = "   👸👸RMilakshsa BoT❤🌻🏻\n\n\nA whatsapp bot developed by\nCYBORG ROBOT" //ur sticker watermark author
+global.wm = "👸👸RMilakshsa BoT❤🌻" //ur watermark
 
 // Other
 global.sessionName = 'session'
